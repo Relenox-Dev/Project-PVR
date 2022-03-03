d963708eee1182fc4aa1d6c954294c5d58eff115
Relenox <dawid.a64@gmail.com>
2022-03-03 19:56:20 +0100

    Basic main window with button effects and list view

---
+++ MVVM/Model/ModModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PVR.MVVM.Model
+{
+	internal class ModModel
+	{
+		public string Name { get; set; }
+		public string Type { get; set; }
+	}
+}
---
+++ MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,50 @@
+﻿using PVR.Core;
+using PVR.MVVM.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PVR.MVVM.ViewModel
+{
+    internal class MainViewModel : ObservableObject
+    {
+        public ObservableCollection<ModModel> Mods { get; set; }
+
+		private ModModel _selectedMod;
+
+		public ModModel SelectedMod
+        {
+			get { return _selectedMod; }
+			set 
+            { 
+                _selectedMod = value;
+                OnPropertyChanged();
+            }
+		}
+
+
+		public MainViewModel()
+		{
+            Mods = new ObservableCollection<ModModel>();
+
+            Mods.Add(new ModModel
+            {
+                Name = "123",
+                Type = "Boots"
+            });
+
+            for (int i = 0; i < 20; i++)
+			{
+                Mods.Add(new ModModel
+                {
+                    Name = $"{i}",
+                    Type = "Furniture"
+                });
+            }
+		}
+
+    }
+}
